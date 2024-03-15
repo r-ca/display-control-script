90dd02b012321efa2ddee32b89d161735c109558
r-ca <nem@nem.x0.to>
2024-03-15 19:51:45 +0900

    Hyprland.py実装 WIP

--- server/executer/hyprland.py
+++ server/executer/hyprland.py
@@ -1,12 +1,40 @@
 from ..interface import IDisplayOperate
+import os
+
+
+class Utils:
+    def __init__(self):
+        self.STATE_FILE = "/tmp/dctrl/hyprland_state"
+
+    def get_state_file_exists(self) -> bool:
+        return os.path.exists(self.STATE_FILE)
+
+    def create_state_file(self):
+        with open(self.STATE_FILE, "w") as f:
+            f.write("1")
+
+    def get_state(self) -> bool:
+        with open(self.STATE_FILE, "r") as f:
+            return f.read() == "1"
+
+    def set_state(self, state: bool):
+        with open(self.STATE_FILE, "w") as f:
+            f.write("1" if state else "0")
+
+    def exec_cmd(self, cmd: str):
+        os.system(cmd)
 
 
 class HyprlandDisplayOperate(IDisplayOperate):
     def state(self) -> bool:
-        pass
+        if (Utils().get_state_file_exists()):
+            return Utils().get_state()
+        else:
+            Utils().create_state_file()
+            return self.state()
 
     def turn_on(self):
-        pass
+        Utils.exec_cmd("hyprctl dispatch dpms on")
 
     def turn_off(self):
-        pass
+        Utils.exec_cmd("hyprctl dispatch dpms off")
