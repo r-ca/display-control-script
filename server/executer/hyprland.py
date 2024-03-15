df0880344fafcf7f76878919ad284928db9c6b38
r-ca <nem@nem.x0.to>
2024-03-15 19:42:14 +0900

    WI

---
+++ server/executer/hyprland.py
@@ -0,0 +1,12 @@
+from ..interface import IDisplayOperate
+
+
+class HyprlandDisplayOperate(IDisplayOperate):
+    def state(self) -> bool:
+        pass
+
+    def turn_on(self):
+        pass
+
+    def turn_off(self):
+        pass
